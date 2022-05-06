c00d6cebea8e59a021a17f65b36e885930f73580
Quentin B <3447731+quentinb69@users.noreply.github.com>
2022-05-06 22:27:37 +0200

    chore/refacto (#13)
    * chore: comment and refacto
    
    * feat: add test
    
    * chore: README badges
    
    * fix: tests
    
    * feat: handler test
    
    * chore: update ci

--- credentials_test.go
+++ credentials_test.go
@@ -8,18 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var cred = Credentials{
-	Username: "Test",
-	Password: "0000",
-	Action:   "none",
-	Csrf:     "none",
-}
-
-const bcrypt0000 = "$2a$05$5Q7AIdXjMaiCnd2VZYNlke7PskIgXNaaOKrUVIa787VUU5L5usooG"
-const bcrypt1111 = "$2a$05$JQKwvqAyG1SzDEr.jkp3Ke1YEDwt1XVkrvjG/0bj5eb8o9CHX0VWi"
-
 func TestIsValid(t *testing.T) {
 	assert := assert.New(t)
+	backup := configuration.Users
+	defer func(){ configuration.Users = backup }()
+
+	cred := credentials
 
 	// No user
 	configuration.Users = nil
@@ -39,12 +33,12 @@ func TestIsValid(t *testing.T) {
 	err = cred.IsValid()
 	assert.EqualError(err, "Credentials : No password supplied for user")
 
-	configuration.Users = map[string]string{"TEST2": "TOTO", cred.Username: bcrypt1111}
+	configuration.Users = map[string]string{"TEST2": "TOTO", cred.Username: globBcrypt1111}
 	err = cred.IsValid()
 	assert.EqualError(err, "crypto/bcrypt: hashedPassword is not the hash of the given password")
 
 	// valid
-	configuration.Users = map[string]string{"TEST2": "TOTO", cred.Username: bcrypt0000}
+	configuration.Users = map[string]string{"TEST2": "TOTO", cred.Username: globBcrypt0000}
 	err = cred.IsValid()
 	assert.NoError(err)
 
@@ -52,13 +46,6 @@ func TestIsValid(t *testing.T) {
 
 func TestGetCredentialsFromForm(t *testing.T) {
 	assert := assert.New(t)
-	username := "user"
-	password := "pass"
-	action := "act"
-	csrf := "cs"
-	dataNoUsername := "action=" + action + "&csrf=" + csrf
-	dataNoPassword := "username=" + username + "&action=" + action + "&csrf=" + csrf
-	data := "username=" + username + "&password=" + password + "&action=" + action + "&csrf=" + csrf
 
 	// no data
 	req, _ := http.NewRequest("POST", "http://localhost", nil)
@@ -67,138 +54,122 @@ func TestGetCredentialsFromForm(t *testing.T) {
 	assert.Nil(c)
 
 	// method GET
-	req, _ = http.NewRequest("GET", "http://localhost?"+data, nil)
+	req, _ = http.NewRequest("GET", "http://localhost?"+globData, nil)
 	c, err = GetCredentialsFromForm(req)
 	assert.EqualError(err, "Credentials : You must send data via POST")
 	assert.Nil(c)
 
 	// no username
-	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(dataNoUsername))
+	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(globDataNoUsername))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	c, err = GetCredentialsFromForm(req)
 	assert.EqualError(err, "Credentials : No username found in Form")
 	assert.Nil(c)
 
 	// no password
-	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(dataNoPassword))
+	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(globDataNoPassword))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	c, err = GetCredentialsFromForm(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username)
+	assert.Equal(c.Username, globUsername)
 	assert.Equal(c.Password, "")
-	assert.Equal(c.Action, action)
-	assert.Equal(c.Csrf, csrf)
+	assert.Equal(c.Action, globAction)
+	assert.Equal(c.Csrf, globCsrf)
 
 	// valid
-	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(data))
+	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(globData))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	c, err = GetCredentialsFromForm(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username)
-	assert.Equal(c.Password, password)
-	assert.Equal(c.Action, action)
-	assert.Equal(c.Csrf, csrf)
+	assert.Equal(c.Username, globUsername)
+	assert.Equal(c.Password, globPassword)
+	assert.Equal(c.Action, globAction)
+	assert.Equal(c.Csrf, globCsrf)
 }
 
 func TestGetCredentialsFromHeader(t *testing.T) {
 	assert := assert.New(t)
-	username := "user"
-	password := "pass"
-	action := "act"
-	csrf := "cs"
-	dataNoUsername := "action=" + action + "&csrf=" + csrf
-	dataNoPassword := "username=" + username + "&action=" + action + "&csrf=" + csrf
-	data := "username=" + username + "&password=" + password + "&action=" + action + "&csrf=" + csrf
-	req, _ := http.NewRequest("POST", "http://localhost", nil)
 
 	// no data
+	req, _ := http.NewRequest("POST", "http://localhost", nil)
 	c, err := GetCredentialsFromHeader(req)
 	assert.Error(err)
 	assert.Nil(c)
 
 	// no username
-	req.Header.Set("Auth-Form", dataNoUsername)
+	req.Header.Set("Auth-Form", globDataNoUsername)
 	c, err = GetCredentialsFromHeader(req)
 	assert.EqualError(err, "Credentials : No username found in Header")
 	assert.Nil(c)
 
 	// no password
-	req.Header.Set("Auth-Form", dataNoPassword)
+	req.Header.Set("Auth-Form", globDataNoPassword)
 	c, err = GetCredentialsFromHeader(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username)
+	assert.Equal(c.Username, globUsername)
 	assert.Equal(c.Password, "")
-	assert.Equal(c.Action, action)
-	assert.Equal(c.Csrf, csrf)
+	assert.Equal(c.Action, globAction)
+	assert.Equal(c.Csrf, globCsrf)
 
 	// valid
-	req.Header.Set("Auth-Form", data)
+	req.Header.Set("Auth-Form", globData)
 	c, err = GetCredentialsFromHeader(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username)
-	assert.Equal(c.Password, password)
-	assert.Equal(c.Action, action)
-	assert.Equal(c.Csrf, csrf)
+	assert.Equal(c.Username, globUsername)
+	assert.Equal(c.Password, globPassword)
+	assert.Equal(c.Action, globAction)
+	assert.Equal(c.Csrf, globCsrf)
 }
 
 func TestGetCredentials(t *testing.T) {
 	assert := assert.New(t)
-	username := cred.Username
-	password := cred.Password
-	passwordH := "1111"
-	action := "act"
-	csrf := "cs"
-	data := "username=" + username + "&password=" + password + "&action=" + action + "&csrf=" + csrf
-	dataHeader := "username=" + username + "H&password=" + passwordH + "&action=" + action + "H&csrf=" + csrf + "H"
-
-	configuration.Users = map[string]string{username: bcrypt0000, username + "H": bcrypt1111}
 
 	// GET
-	req, _ := http.NewRequest("GET", "http://localhost/?"+data, nil)
+	req, _ := http.NewRequest("GET", "http://localhost/?"+globData, nil)
 	c, err := GetCredentials(req)
 	assert.EqualError(err, "Credentials : You must send data via POST")
 	assert.Nil(c)
 
 	// POST
-	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(data))
+	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(globData))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	c, err = GetCredentials(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username)
-	assert.Equal(c.Password, password)
-	assert.Equal(c.Action, action)
-	assert.Equal(c.Csrf, csrf)
+	assert.Equal(c.Username, globUsername)
+	assert.Equal(c.Password, globPassword)
+	assert.Equal(c.Action, globAction)
+	assert.Equal(c.Csrf, globCsrf)
 
 	// HEADER
-	req.Header.Set("Auth-Form", dataHeader)
+	req.Header.Set("Auth-Form", globDataHeader)
 	c, err = GetCredentials(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username+"H")
-	assert.Equal(c.Password, passwordH)
-	assert.Equal(c.Action, action+"H")
-	assert.Equal(c.Csrf, csrf+"H")
+	assert.Equal(c.Username, globUsername+"H")
+	assert.Equal(c.Password, globPasswordH)
+	assert.Equal(c.Action, globAction+"H")
+	assert.Equal(c.Csrf, globCsrf+"H")
 
 	// POST & HEADER, valid HEADER
-	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(data))
+	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(globData))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Auth-Form", dataHeader)
+	req.Header.Add("Auth-Form", globDataHeader)
 	c, err = GetCredentials(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username+"H")
-	assert.Equal(c.Password, passwordH)
-	assert.Equal(c.Action, action+"H")
-	assert.Equal(c.Csrf, csrf+"H")
+	assert.Equal(c.Username, globUsername+"H")
+	assert.Equal(c.Password, globPasswordH)
+	assert.Equal(c.Action, globAction+"H")
+	assert.Equal(c.Csrf, globCsrf+"H")
 
 	// POST & HEADER, invalid HEADER
-	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(data))
+	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader(globData))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("Auth-Form", "FAKE")
 	c, err = GetCredentials(req)
 	assert.NoError(err)
-	assert.Equal(c.Username, username)
-	assert.Equal(c.Password, password)
-	assert.Equal(c.Action, action)
-	assert.Equal(c.Csrf, csrf)
+	assert.Equal(c.Username, globUsername)
+	assert.Equal(c.Password, globPassword)
+	assert.Equal(c.Action, globAction)
+	assert.Equal(c.Csrf, globCsrf)
 
 	// POST & HEADER, invalid HEADER and invalid POST
 	req, _ = http.NewRequest("POST", "http://localhost", strings.NewReader("FAKE"))
@@ -207,5 +178,4 @@ func TestGetCredentials(t *testing.T) {
 	c, err = GetCredentials(req)
 	assert.EqualError(err, "Credentials : No username found in Form")
 	assert.Nil(c)
-
 }
